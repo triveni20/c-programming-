e5f7b0b4c75a43c82b2c2610bec907dcd9b26c0f
triveni20 <87942077+triveni20@users.noreply.github.com>
2021-07-29 20:19:59 +0530

    Update file1.c

--- file1.c
+++ file1.c
@@ -1,17 +1,38 @@
 #incude<stdio.h>
+#include<stdlib.h>
 #include<string.h>
 int main()
 {
     FILE *fp;
-    fp=fopen("\ntriveni\doccuments\ls.c\n","a");
+    fp=fopen("\ntriveni\doccuments\ls.c\n","r");
+    fp=fopen("\ntriveni\doccuments\ls.c\n","w");
+	
     if(fp==NULL)
        {
 	  printf("ls.c file is failed to opened ");
-	}
-     else 
-       {
-          printf("the file is now opened\n");
+	exit(1);
        }
+      while(fscanf(fp,"%d",&i)==1)
+       {
+	      fscanf(stdin,"%d %d\n",i);
+        }
+	fclose(fp);
+	printf("file should be closed");
+	return 0;
+}
+	
+		    
+		   
+		   
+		   
+		   
+	
+	
+	
+         
+	
+     
+     
        return 0;
        
  }
